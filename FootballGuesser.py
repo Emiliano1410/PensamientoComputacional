a5ab4971d4279ec3b1106ec488fb356e3fffeda2
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2023-08-24 18:19:43 -0600

    FootballGuesser.py

--- FootballGuesser.py
+++ FootballGuesser.py
@@ -1,1 +1,17 @@
+#Pedir los nombres de los equipos
+EQUIPO1 = input("Ingrese el nombre del EQUIPO 1: ")
+EQUIPO2 = input("Ingrese el nombre del EQUIPO2: ")
+
+#Pedir las estadisticas del EQUIPO1
+victorias1 = int(input(f"Ingrese el numero de victorias de {EQUIPO1}: "))
+derrotas1 = int(input(f"Ingrese el numero de derrotas de {EQUIPO1}: "))
+empates1 = int(input(f"Ingrese el numero de empates de {EQUIPO1}: "))
+anotados1 = int(input(f"Ingrese el numero de goles anotados de {EQUIPO1}: "))
+contra1 = int(input(f"Ingrese el numero de goles en contra de {EQUIPO1}: "))
+puntos1 = int(input(f"Ingrese el numero de puntos del torneo de {EQUIPO1}: "))
+partidos1 = int(input(f"Ingrese el numero de victorias en los ultimos cinco partidos de {EQUIPO1}: "))
+dcinco1 = int(input(f"Ingrese el numero de derrotas en los ultimos cinco partidos de {EQUIPO1}: "))
+titulares1 = int(input(f"Ingrese el numero de jugadores titulares lesionados de {EQUIPO1}: "))
+promedio1 = float(input(f"Ingrese el promedio de goles por partido de {EQUIPO1}: "))
+
 
