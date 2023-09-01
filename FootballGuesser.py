91680a765786b8874cfd38e790398519e4a9cc51
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2023-09-01 15:09:26 -0600

    FootballGuesser.py

--- FootballGuesser.py
+++ FootballGuesser.py
@@ -1,18 +1,19 @@
-#Pedir los nombres de los equipos
-EQUIPO1 = input("Ingrese el nombre del EQUIPO 1: ")
-EQUIPO2 = input("Ingrese el nombre del EQUIPO2: ")
-
-#Pedir las estadisticas del EQUIPO1
-victorias1 = int(input(f"Ingrese el número de victorias de {EQUIPO1}: "))
-derrotas1 = int(input(f"Ingrese el número de derrotas de {EQUIPO1}: "))
-empates1 = int(input(f"Ingrese el número de empates de {EQUIPO1}: "))
-anotados1 = int(input(f"Ingrese el número de goles anotados por {EQUIPO1}: "))
-contra1 = int(input(f"Ingrese el número de goles en contra por {EQUIPO1}: "))
-puntos1 = int(input(f"Ingrese el número de puntos del torneo para {EQUIPO1}: "))
-partidos1 = int(input(f"Ingrese el número de victorias en los ultimos cinco partidos para {EQUIPO1}: "))
-dcinco1 = int(input(f"Ingrese el número de derrotas en los últimos cinco partidos para {EQUIPO1}: "))
-titulares1 = int(input(f"Ingrese el número de jugadores titulares lesionados para {EQUIPO1}: "))
-promedio1 = float(input(f"Ingrese el promedio de goles por partido para {EQUIPO1}: "))
+#Pedir las estadisticas de los equipos
+def obtener_estadisticas_equipo(nombre_equipo):
+    victorias = int(input(f"Ingrese el número de victorias de {nombre_equipo}: "))
+    derrotas = int(input(f"Ingrese el número de derrotas de {nombre_equipo}: "))
+    empates = int(input(f"Ingrese el número de empates de {nombre_equipo}: "))
+    anotados = int(input(f"Ingrese el número de goles anotados por {nombre_equipo}: "))
+    contra = int(input(f"Ingrese el número de goles en contra por {nombre_equipo}: "))
+    puntos = int(input(f"Ingrese el número de puntos del torneo para {nombre_equipo}: "))
+    vcinco = int(input(f"Ingrese el número de victorias en los últimos cinco partidos para {nombre_equipo}: "))
+    dcinco = int(input(f"Ingrese el número de derrotas en los últimos cinco partidos para {nombre_equipo}: "))
+    titulares = int(input(f"Ingrese el número de jugadores titulares lesionados para {nombre_equipo}: "))
+    promedio = float(input(f"Ingrese el promedio de goles por partido para {nombre_equipo}: "))
+    tiros_arco = int(input(f"Ingrese el número de tiros al arco por partido para {nombre_equipo}: "))
+    posesion = float(input(f"Ingrese el promedio de  porcentaje de posesión del balón por partido para {nombre_equipo}: "))
+    paradas_portero = int(input(f"Ingrese el promedio de paradas del portero por partido para {nombre_equipo}: "))
+    return victorias, derrotas, empates, anotados, contra, puntos, vcinco, dcinco, titulares, promedio, tiros_arco, posesion, paradas_portero
 
 #Pedir las estadisticas del EQUIPO2
 victorias2 = int(input(f"Ingrese el número de victorias de {EQUIPO2}: "))
