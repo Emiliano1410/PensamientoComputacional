3946abfb8b2c6f59da6d3ef488dce7d1f070520a
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2023-09-01 16:01:14 -0600

    FootballGuesser.py

--- FootballGuesser.py
+++ FootballGuesser.py
@@ -1,21 +1,19 @@
-#Pedir las estadisticas de los equipos
 def obtener_estadisticas_equipo(nombre_equipo):
     victorias = int(input(f"Ingrese el número de victorias de {nombre_equipo}: "))
     derrotas = int(input(f"Ingrese el número de derrotas de {nombre_equipo}: "))
     empates = int(input(f"Ingrese el número de empates de {nombre_equipo}: "))
     anotados = int(input(f"Ingrese el número de goles anotados por {nombre_equipo}: "))
     contra = int(input(f"Ingrese el número de goles en contra de {nombre_equipo}: "))
-    puntos = int(input(f"Ingrese el número de puntos del torneo de {nombre_equipo}: "))
-    vcinco = int(input(f"Ingrese el número de victorias en los últimos cinco partidos de {nombre_equipo}: "))
-    dcinco = int(input(f"Ingrese el número de derrotas en los últimos cinco partidos de {nombre_equipo}: "))
-    titulares = int(input(f"Ingrese el número de jugadores titulares lesionados de {nombre_equipo}: "))
-    promedio = float(input(f"Ingrese el promedio de goles por partido de {nombre_equipo}: "))
-    tiros_arco = int(input(f"Ingrese el número de tiros al arco por partido de {nombre_equipo}: "))
-    posesion = float(input(f"Ingrese el promedio de  porcentaje de posesión del balón por partido de {nombre_equipo}: "))
-    paradas_portero = int(input(f"Ingrese el promedio de paradas del portero por partido de {nombre_equipo}: "))
+    puntos = int(input(f"Ingrese el número de puntos del torneo para {nombre_equipo}: "))
+    vcinco = int(input(f"Ingrese el número de victorias en los últimos cinco partidos para {nombre_equipo}: "))
+    dcinco = int(input(f"Ingrese el número de derrotas en los últimos cinco partidos para {nombre_equipo}: "))
+    titulares = int(input(f"Ingrese el número de jugadores titulares lesionados para {nombre_equipo}: "))
+    promedio = float(input(f"Ingrese el promedio de goles por partido para {nombre_equipo}: "))
+    tiros_arco = int(input(f"Ingrese el número de tiros al arco por partido para {nombre_equipo}: "))
+    posesion = float(input(f"Ingrese el promedio de  porcentaje de posesión del balón por partido para {nombre_equipo}: "))
+    paradas_portero = int(input(f"Ingrese el promedio de paradas del portero por partido para {nombre_equipo}: "))
     return victorias, derrotas, empates, anotados, contra, puntos, vcinco, dcinco, titulares, promedio, tiros_arco, posesion, paradas_portero
 
-# Calcular el puntaje
 def calcular_puntaje(victorias, derrotas, empates, anotados, contra, puntos, vcinco, dcinco, titulares, promedio, tiros_arco, posesion, paradas_portero):
     ponderacion_victorias = 3
     ponderacion_derrotas = -1
@@ -49,19 +47,26 @@ def calcular_puntaje(victorias, derrotas, empates, anotados, contra, puntos, vci
     
     return puntaje
 
-EQUIPO1 = input("Ingrese el nombre del EQUIPO 1: ")
-estadisticas1 = obtener_estadisticas_equipo(EQUIPO1)
+def determinar_resultado(puntaje_equipo1, puntaje_equipo2):
+    if puntaje_equipo1 > puntaje_equipo2:
+        return f"{EQUIPO1} GANA"
+    elif puntaje_equipo2 > puntaje_equipo1:
+        return f"{EQUIPO2} GANA"
+    else:
+        return "EMPATE"
 
-EQUIPO2 = input("Ingrese el nombre del EQUIPO 2: ")
-estadisticas2 = obtener_estadisticas_equipo(EQUIPO2)
+while True:
+  EQUIPO1 = input("Ingrese el nombre del EQUIPO 1: ")
+  victorias1, derrotas1, empates1, anotados1, contra1, puntos1, partidos1, dcinco1, titulares1, promedio1, tiros_arco1, posesion1, paradas_portero1 = obtener_estadisticas_equipo(EQUIPO1)
 
-PUNTAJE1 = calcular_puntaje(estadisticas1)
-PUNTAJE2 = calcular_puntaje(estadisticas2) 
+  EQUIPO2 = input("Ingrese el nombre del EQUIPO 2: ")
+  victorias2, derrotas2, empates2, anotados2, contra2, puntos2, partidos2, dcinco2, titulares2, promedio2, tiros_arco2, posesion2, paradas_portero2 = obtener_estadisticas_equipo(EQUIPO2)
 
-# Comparar puntajes y mostrar resultado
-if PUNTAJE1 > PUNTAJE2:
-    print(f"{EQUIPO1} GANA")
-elif PUNTAJE2 > PUNTAJE1:
-    print(f"{EQUIPO2} GANA")
-else:
-    print("EMPATE")
+  PUNTAJE1 = calcular_puntaje(victorias1, derrotas1, empates1, anotados1, contra1, puntos1, partidos1, dcinco1, titulares1, promedio1, tiros_arco1, posesion1, paradas_portero1)
+  PUNTAJE2 = calcular_puntaje(victorias2, derrotas2, empates2, anotados2, contra2, puntos2, partidos2, dcinco2, titulares2, promedio2, tiros_arco2, posesion2, paradas_portero2)
+
+  resultado = determinar_resultado(PUNTAJE1, PUNTAJE2)
+  print(resultado)
+  respuesta = input("¿Desea predecir el resultado de otro partido? (si/no): ").strip().lower()
+  if respuesta != "si":
+        break
