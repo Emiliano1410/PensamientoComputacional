a132894b24522a2252f9401dffffc6acf407ca70
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2023-10-05 20:14:57 -0600

    FootballGuesser.py

--- FootballGuesser.py
+++ FootballGuesser.py
@@ -1,5 +1,6 @@
 def obtener_estadisticas_equipo(nombre_equipo):
-     
+    estadisticas_equipo = []
+
     while True:
         try:
             victorias = int(input(f"Ingrese el número de victorias de {nombre_equipo}: "))
@@ -15,11 +16,15 @@ def obtener_estadisticas_equipo(nombre_equipo):
             tiros_arco = float(input(f"Ingrese el promedio de tiros al arco por partido de {nombre_equipo}: "))
             posesion = float(input(f"Ingrese el promedio de  porcentaje de posesión del balón por partido de {nombre_equipo}: "))
             paradas_portero = float(input(f"Ingrese el promedio de paradas del portero por partido de {nombre_equipo}: "))
-            victorias_ult5_entre_ellos= int(input(f"Ingrese el número de victorias en los últimos cinco enfrentamientos entre estos equipos para {nombre_equipo}: "))
-            return victorias, derrotas, empates, anotados, contra, puntos, vcinco, dcinco, titulares, promedio, tiros_arco, posesion, paradas_portero, victorias_ult5_entre_ellos
+            victorias_ult5_entre_ellos = int(input(f"Ingrese el número de victorias en los últimos cinco enfrentamientos entre estos equipos para {nombre_equipo}: "))
+            
+            estadisticas_equipo = [victorias, derrotas, empates, anotados, contra, puntos, vcinco, dcinco, titulares, promedio, tiros_arco, posesion, paradas_portero, victorias_ult5_entre_ellos]
+            
             break
         except ValueError:
             print("¡Error! Ingrese un valor válido.")
+
+    return estadisticas_equipo
 
 def calcular_puntaje(victorias, derrotas, empates, anotados, contra, puntos, vcinco, dcinco, titulares, promedio, tiros_arco, posesion, paradas_portero, victorias_ult5_entre_ellos):
     ponderacion_victorias = 3
@@ -58,8 +63,6 @@ def calcular_puntaje(victorias, derrotas, empates, anotados, contra, puntos, vci
 
 import math
 
-import math
-
 def calcular_probabilidades(puntaje_equipo1, puntaje_equipo2):
     probabilidad_victoria_equipo1 = 1 / (1 + math.exp((puntaje_equipo2 - puntaje_equipo1) / 100))
     probabilidad_victoria_equipo2 = 1 / (1 + math.exp((puntaje_equipo1 - puntaje_equipo2) / 100))
@@ -81,30 +84,30 @@ def calcular_probabilidades(puntaje_equipo1, puntaje_equipo2):
 
 def determinar_resultado(puntaje_equipo1, puntaje_equipo2):
     if puntaje_equipo1 > puntaje_equipo2:
-        return f"{EQUIPO1} GANA"
+        return f"{EQUIPO1} Gana"
     elif puntaje_equipo2 > puntaje_equipo1:
-        return f"{EQUIPO2} GANA"
+        return f"{EQUIPO2} Gana"
     else:
-        return "EMPATE"
+        return "Empate"
 
 while True:
-  EQUIPO1 = input("Ingrese el nombre del EQUIPO 1: ")
-  victorias1, derrotas1, empates1, anotados1, contra1, puntos1, partidos1, dcinco1, titulares1, promedio1, tiros_arco1, posesion1, paradas_portero1, victorias_ult5_entre_ellos1 = obtener_estadisticas_equipo(EQUIPO1)
+    EQUIPO1 = input("Ingrese el nombre del EQUIPO 1: ")
+    estadisticas_equipo1 = obtener_estadisticas_equipo(EQUIPO1)
 
-  EQUIPO2 = input("Ingrese el nombre del EQUIPO 2: ")
-  victorias2, derrotas2, empates2, anotados2, contra2, puntos2, partidos2, dcinco2, titulares2, promedio2, tiros_arco2, posesion2, paradas_portero2, victorias_ult5_entre_ellos2 = obtener_estadisticas_equipo(EQUIPO2)
+    EQUIPO2 = input("Ingrese el nombre del EQUIPO 2: ")
+    estadisticas_equipo2 = obtener_estadisticas_equipo(EQUIPO2)
 
-  PUNTAJE1 = calcular_puntaje(victorias1, derrotas1, empates1, anotados1, contra1, puntos1, partidos1, dcinco1, titulares1, promedio1, tiros_arco1, posesion1, paradas_portero1, victorias_ult5_entre_ellos1)
-  PUNTAJE2 = calcular_puntaje(victorias2, derrotas2, empates2, anotados2, contra2, puntos2, partidos2, dcinco2, titulares2, promedio2, tiros_arco2, posesion2, paradas_portero2, victorias_ult5_entre_ellos2)
+    PUNTAJE1 = calcular_puntaje(*estadisticas_equipo1)
+    PUNTAJE2 = calcular_puntaje(*estadisticas_equipo2)
 
-  resultado = determinar_resultado(PUNTAJE1, PUNTAJE2)
-  probabilidad_victoria_equipo1, probabilidad_empate, probabilidad_victoria_equipo2 = calcular_probabilidades(PUNTAJE1, PUNTAJE2)
-  
-  print(f"Predicción: {resultado}")
-  print(f"Probabilidad de victoria de {EQUIPO1}: {probabilidad_victoria_equipo1:.2f}%")
-  print(f"Probabilidad de empate: {probabilidad_empate:.2f}%")
-  print(f"Probabilidad de victoria de {EQUIPO2}: {probabilidad_victoria_equipo2:.2f}%")
-  
-  respuesta = input("¿Desea predecir el resultado de otro partido? (si/no): ").strip().lower()
-  if respuesta != "si":
+    resultado = determinar_resultado(PUNTAJE1, PUNTAJE2)
+    probabilidad_victoria_equipo1, probabilidad_empate, probabilidad_victoria_equipo2 = calcular_probabilidades(PUNTAJE1, PUNTAJE2)
+
+    print(f"Predicción: {resultado}")
+    print(f"Probabilidad de victoria de {EQUIPO1}: {probabilidad_victoria_equipo1:.2f}%")
+    print(f"Probabilidad de empate: {probabilidad_empate:.2f}%")
+    print(f"Probabilidad de victoria de {EQUIPO2}: {probabilidad_victoria_equipo2:.2f}%")
+
+    respuesta = input("¿Desea predecir el resultado de otro partido? (si/no): ").strip().lower()
+    if respuesta != "si":
         break
