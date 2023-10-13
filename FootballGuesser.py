f93ed6125d7fde71950d3b2e77bc3a2d5e09b944
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2023-10-13 16:10:03 -0600

    FootballGuesser.py

--- FootballGuesser.py
+++ FootballGuesser.py
@@ -1,24 +1,22 @@
 def obtener_estadisticas_equipo(nombre_equipo):
-    estadisticas_equipo = []
+    estadisticas_equipo = {}
 
     while True:
         try:
-            victorias = int(input(f"Ingrese el número de victorias de {nombre_equipo}: "))
-            derrotas = int(input(f"Ingrese el número de derrotas de {nombre_equipo}: "))
-            empates = int(input(f"Ingrese el número de empates de {nombre_equipo}: "))
-            anotados = int(input(f"Ingrese el número de goles anotados por {nombre_equipo}: "))
-            contra = int(input(f"Ingrese el número de goles en contra de {nombre_equipo}: "))
-            puntos = int(input(f"Ingrese el número de puntos del torneo de {nombre_equipo}: "))
-            vcinco = int(input(f"Ingrese el número de victorias en los últimos cinco partidos de {nombre_equipo}: "))
-            dcinco = int(input(f"Ingrese el número de derrotas en los últimos cinco partidos de {nombre_equipo}: "))
-            titulares = int(input(f"Ingrese el número de jugadores titulares lesionados de {nombre_equipo}: "))
-            promedio = float(input(f"Ingrese el promedio de goles por partido de {nombre_equipo}: "))
-            tiros_arco = float(input(f"Ingrese el promedio de tiros al arco por partido de {nombre_equipo}: "))
-            posesion = float(input(f"Ingrese el promedio de  porcentaje de posesión del balón por partido de {nombre_equipo}: "))
-            paradas_portero = float(input(f"Ingrese el promedio de paradas del portero por partido de {nombre_equipo}: "))
-            victorias_ult5_entre_ellos = int(input(f"Ingrese el número de victorias en los últimos cinco enfrentamientos entre estos equipos para {nombre_equipo}: "))
-            
-            estadisticas_equipo = [victorias, derrotas, empates, anotados, contra, puntos, vcinco, dcinco, titulares, promedio, tiros_arco, posesion, paradas_portero, victorias_ult5_entre_ellos]
+            estadisticas_equipo['victorias'] = int(input(f"Ingrese el número de victorias de {nombre_equipo}: "))
+            estadisticas_equipo['derrotas'] = int(input(f"Ingrese el número de derrotas de {nombre_equipo}: "))
+            estadisticas_equipo['empates'] = int(input(f"Ingrese el número de empates de {nombre_equipo}: "))
+            estadisticas_equipo['anotados'] = int(input(f"Ingrese el número de goles anotados por {nombre_equipo}: "))
+            estadisticas_equipo['contra'] = int(input(f"Ingrese el número de goles en contra de {nombre_equipo}: "))
+            estadisticas_equipo['puntos'] = int(input(f"Ingrese el número de puntos del torneo de {nombre_equipo}: "))
+            estadisticas_equipo['vcinco'] = int(input(f"Ingrese el número de victorias en los últimos cinco partidos de {nombre_equipo}: "))
+            estadisticas_equipo['dcinco'] = int(input(f"Ingrese el número de derrotas en los últimos cinco partidos de {nombre_equipo}: "))
+            estadisticas_equipo['titulares'] = int(input(f"Ingrese el número de jugadores titulares lesionados de {nombre_equipo}: "))
+            estadisticas_equipo['promedio'] = float(input(f"Ingrese el promedio de goles por partido de {nombre_equipo}: "))
+            estadisticas_equipo['tiros_arco'] = float(input(f"Ingrese el promedio de tiros al arco por partido de {nombre_equipo}: "))
+            estadisticas_equipo['posesion'] = float(input(f"Ingrese el promedio de porcentaje de posesión del balón por partido de {nombre_equipo}: "))
+            estadisticas_equipo['paradas_portero'] = float(input(f"Ingrese el promedio de paradas del portero por partido de {nombre_equipo}: "))
+            estadisticas_equipo['victorias_ult5_entre_ellos'] = int(input(f"Ingrese el número de victorias en los últimos cinco enfrentamientos entre estos equipos para {nombre_equipo}: "))
             
             break
         except ValueError:
@@ -26,7 +24,7 @@ def obtener_estadisticas_equipo(nombre_equipo):
 
     return estadisticas_equipo
 
-def calcular_puntaje(victorias, derrotas, empates, anotados, contra, puntos, vcinco, dcinco, titulares, promedio, tiros_arco, posesion, paradas_portero, victorias_ult5_entre_ellos):
+def calcular_puntaje(estadisticas_equipo):
     ponderacion_victorias = 3
     ponderacion_derrotas = -1
     ponderacion_empates = 1
@@ -43,20 +41,20 @@ def calcular_puntaje(victorias, derrotas, empates, anotados, contra, puntos, vci
     ponderacion_ult5_entre_ellos = 5
     
     puntaje = (
-        victorias * ponderacion_victorias
-        + derrotas * ponderacion_derrotas
-        + empates * ponderacion_empates
-        + anotados * ponderacion_anotados
-        + contra * ponderacion_contra
-        + puntos * ponderacion_puntos
-        + vcinco * ponderacion_vcinco
-        + dcinco * ponderacion_dcinco
-        + titulares * ponderacion_titulares
-        + promedio * ponderacion_promedio
-        + tiros_arco * ponderacion_tiros_arco
-        + posesion * ponderacion_posesion
-        + paradas_portero * ponderacion_paradas_portero
-        + victorias_ult5_entre_ellos * ponderacion_ult5_entre_ellos
+        estadisticas_equipo['victorias'] * ponderacion_victorias
+        + estadisticas_equipo['derrotas'] * ponderacion_derrotas
+        + estadisticas_equipo['empates'] * ponderacion_empates
+        + estadisticas_equipo['anotados'] * ponderacion_anotados
+        + estadisticas_equipo['contra'] * ponderacion_contra
+        + estadisticas_equipo['puntos'] * ponderacion_puntos
+        + estadisticas_equipo['vcinco'] * ponderacion_vcinco
+        + estadisticas_equipo['dcinco'] * ponderacion_dcinco
+        + estadisticas_equipo['titulares'] * ponderacion_titulares
+        + estadisticas_equipo['promedio'] * ponderacion_promedio
+        + estadisticas_equipo['tiros_arco'] * ponderacion_tiros_arco
+        + estadisticas_equipo['posesion'] * ponderacion_posesion
+        + estadisticas_equipo['paradas_portero'] * ponderacion_paradas_portero
+        + estadisticas_equipo['victorias_ult5_entre_ellos'] * ponderacion_ult5_entre_ellos
     )
     
     return puntaje
